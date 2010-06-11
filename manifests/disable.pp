6e977607c76cfc1e348a086ce598ec644cc19b04
Matthias Imsand <imsand@puzzle.ch>
2010-06-11 15:43:55 +0200

    bugfix smartd disable

--- manifests/disable.pp
+++ manifests/disable.pp
@@ -4,4 +4,10 @@ class smartd::disable inherits smartd::base{
     Package['smartmontools']{
         ensure => absent,
     }
+    Service['smartd']{
+        ensure => stopped,
+        enable => false,
+        hasstatus => true,
+        require => Package['smartmontools'],
+    }
 }
