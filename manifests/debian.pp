6c6f33c480d776c482468656c05f58e48a40345c
mh <mh@immerda.ch>
2010-12-10 10:26:14 +0100

    enable smartmontools on debian

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -2,4 +2,10 @@ class smartd::debian inherits smartd::base {
   Service['smartd']{
     name => 'smartmontools',
   }
+  line{'enable_smartmontools':
+    file => '/etc/default/smartmontools',
+    line => 'start_smartd=yes',
+    require => Package['smartmontools'],
+    notify => Service['smartd'],
+  }
 }
