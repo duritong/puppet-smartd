ddeb767beff65023bc456d25ee3a600f779261ab
mh <mh@immerda.ch>
2010-12-10 11:13:25 +0100

    add pattern as we don't have a status method

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -1,6 +1,8 @@
 class smartd::debian inherits smartd::base {
   Service['smartd']{
     name => 'smartmontools',
+    pattern => 'smartd',
+    hasstatus => false,
   }
   line{'enable_smartmontools':
     file => '/etc/default/smartmontools',
