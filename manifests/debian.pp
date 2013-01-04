3ba256b44de9b75f1b59500c4ca02bda6b255d8b
mh <mh@immerda.ch>
2013-01-04 20:16:53 +0100

    use file_line instead of line

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -1,13 +1,14 @@
+# on debian + enable tools
 class smartd::debian inherits smartd::base {
   Service['smartd']{
-    name => 'smartmontools',
-    pattern => 'smartd',
+    name      => 'smartmontools',
+    pattern   => 'smartd',
     hasstatus => false,
   }
-  line{'enable_smartmontools':
-    file => '/etc/default/smartmontools',
-    line => 'start_smartd=yes',
+  file_line{'enable_smartmontools':
+    path    => '/etc/default/smartmontools',
+    line    => 'start_smartd=yes',
     require => Package['smartmontools'],
-    notify => Service['smartd'],
+    notify  => Service['smartd'],
   }
 }
